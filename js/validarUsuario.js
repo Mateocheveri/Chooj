a3a4bb7d8bad50c1ab8d85ceb15a19b0ee02f62d
mateocheveri <cros746@gmail.com>
2025-03-31 15:06:15 -0500

    nav-bar funcional 31-03

--- js/validarUsuario.js
+++ js/validarUsuario.js
@@ -1,28 +1,34 @@
-let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
-
-
-// Mostrar u ocultar botones cuando el usuario inicie sesión 
-export function validarSesion() {
+document.addEventListener("DOMContentLoaded", function () {
   const btnRegistro = document.querySelector('#btnregistro');
   const btnIniciarSesion = document.querySelector('#btniniciar');
   const btnModulos = document.querySelector('#btnmodulos');
   const userIcon = document.querySelector("#ico");
 
 
-  for (let i = 0; i < usuarios.length; i++) {
-      if (usuarios[i].logged) {
-          btnModulos.style.display = "block";
-          userIcon.style.display = "block";
-          btnRegistro.style.display = "none";
-          btnIniciarSesion.style.display = "none";
 
-          return;
-      }
+
+  let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+
+  for (let i = 0; i < usuarios.length; i++) {
+
+    let confirmarSesion = usuarios[i] ? usuarios[i].logged : false
+
+    console.log(confirmarSesion)
+
+    if (confirmarSesion) {
+
+      btnModulos.style.display = "block";
+      btnRegistro.style.display = "none";
+      btnIniciarSesion.style.display = "none";
+
+      return
+    }
+
   }
 
-  // Si no hay sesión iniciada
+ 
   btnModulos.style.display = "none";
   userIcon.style.display = "none";
   btnRegistro.style.display = "block";
   btnIniciarSesion.style.display = "block";
-}
+});

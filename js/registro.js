6ef96f22d83fba74acb662508dac721dce4dbb6a
mateocheveri <cros746@gmail.com>
2025-04-08 14:29:17 -0500

    arreglo del popup 8-04

--- js/registro.js
+++ js/registro.js
@@ -19,9 +19,9 @@ function registrarUser(e){
         const myModal = document.getElementById('myModal')
         const myInput = document.getElementById('myInput')
             
-        myModal.addEventListener('shown.bs.modal', () => {
+        /* myModal.addEventListener('shown.bs.modal', () => {
           myInput.focus()
-        })
+        }) */
         return
       }      
     }
